901a2cdee2faec50855030609707fec94df30632
Aaron Schlesinger <arschles@gmail.com>
2013-03-27 17:11:34 -0700

    splitting extension classes into separate files

---
+++ src/main/scala/com/stackmob/customcode/localrunner/sdk/data/SMObjectW.scala
@@ -0,0 +1,17 @@
+package com.stackmob.customcode.localrunner.sdk.data
+
+import com.stackmob.sdkapi.SMObject
+
+/**
+ * Created by IntelliJ IDEA.
+ * 
+ * com.stackmob.customcode.localrunner.sdk.data
+ * 
+ * User: aaron
+ * Date: 3/27/13
+ * Time: 5:09 PM
+ */
+trait SMObjectW {
+  protected def smObject: SMObject
+
+}
