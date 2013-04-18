b285520e0b0cdbdc9e0cec467b20f70192d2811e
Aaron Schlesinger <arschles@gmail.com>
2013-04-17 17:54:49 -0700

    ftb

--- src/main/scala/com/stackmob/customcode/localrunner/sdk/data/TooManyDataServiceCallsException.scala
+++ src/main/scala/com/stackmob/customcode/localrunner/sdk/data/TooManyDataServiceCallsException.scala
@@ -9,11 +9,14 @@ package com.stackmob.customcode.localrunner.sdk.data
  * Date: 4/17/13
  * Time: 4:22 PM
  */
-class TooManyDataServiceCallsException(max: Int, calls: Seq[String]) extends Exception("""
-    You've made more than %d calls to DataService in this request. Doing so will cause your custom code to be
+class TooManyDataServiceCallsException(max: Int, calls: Seq[String]) extends Exception {
+  val callList = calls.mkString("\n")
+  s"""
+    You've made more than $max calls to DataService in this request. Doing so will cause your custom code to be
     slow and possibly take too many resources. Please reduce the number of calls you're making.
     Here are the calls that you've made, in order:
-    %s""".format(max, calls.mkString("\n")))
+    $callList"""
+}
 
 object TooManyDataServiceCallsException {
   def apply(max: Int, calls: Seq[String]) = new TooManyDataServiceCallsException(max, calls)
