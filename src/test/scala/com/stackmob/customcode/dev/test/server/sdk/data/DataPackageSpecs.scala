dd2fb7caca59780edfcfe342cabe08b7599bcc5e
Aaron Schlesinger <arschles@gmail.com>
2013-04-30 18:18:48 -0700

    ftb part 2

--- src/test/scala/com/stackmob/customcode/dev/test/server/sdk/data/DataPackageSpecs.scala
+++ src/test/scala/com/stackmob/customcode/dev/test/server/sdk/data/DataPackageSpecs.scala
@@ -3,6 +3,8 @@ package com.stackmob.customcode.dev.test.server.sdk.data
 import org.specs2.Specification
 import com.stackmob.customcode.dev.server.sdk.data._
 import com.stackmob.sdkapi._
+import collection.JavaConverters._
+import org.specs2.matcher.MatchResult
 
 class DataPackageSpecs extends Specification { def is =
   "DataPackageSpecs".title                                                                                              ^ end ^
@@ -12,7 +14,7 @@ class DataPackageSpecs extends Specification { def is =
     "work properly for booleans"                                                                                        ! checkSMValue(true, new SMBoolean(true)) ^
     "work properly for Longs"                                                                                           ! checkSMValue(1L, new SMInt(1L)) ^
     "work properly for Doubles"                                                                                         ! checkSMValue(1D, new SMDouble(1D)) ^
-    "work properly for Ints"                                                                                            ! checkSMValue(1, new SMLong(1L)) ^
+    "work properly for Ints"                                                                                            ! checkSMValue(1, new SMInt(1L)) ^
     "work properly for Strings"                                                                                         ! checkSMValue("a", new SMString("a")) ^
     "work properly for lists of primitives"                                                                             ! checkSMValuePrimitiveList ^
     "work properly for nested lists"                                                                                    ! pending ^
@@ -23,12 +25,13 @@ class DataPackageSpecs extends Specification { def is =
                                                                                                                         end ^
                                                                                                                         end
   private def smBodyTest = {
-    val smInc = new SMIncrement("testField", 1)
-    val smSet = new SMSet("testField", new SMInt(1))
-    val smUpdates = List(smInc, smSet)
+    val smInc = new SMIncrement("testInc", 1)
+    val smSet = new SMSet("testSet", new SMInt(1))
+    val smUpdates = List[SMUpdate](smInc, smSet)
     val res = smBody(smUpdates)
-    val expectedMap = Map(s"${smInc.getField}[inc]" -> smInc.getValue, smSet.getField -> smSet.getValue)
-    res.toList must haveTheSameElementsAs(expectedMap)
+    val expectedMap = Map(s"${smInc.getField}[inc]" -> smInc.getValue.getValue.toString,
+      smSet.getField -> smSet.getValue.getValue.toString)
+    res must haveTheSameElementsAs(expectedMap)
   }
 
   /**
@@ -36,15 +39,18 @@ class DataPackageSpecs extends Specification { def is =
    * @param t the raw value to be converted
    * @param smT the SMValue that should result from the conversion
    * @return the MatchResult representing whether the conversion succeeded
+   * @note because SMValue is invariant in its type {{T}}, we have to cast everything up
+   *       to {{Any}} in order to make this code compile
    */
-  def checkSMValue(t: Any, smT: SMValue[_]) = {
-    smValue(t) must beEqualTo(smT)
+  def checkSMValue(t: Any, smT: SMValue[_]): MatchResult[Any] = {
+    val res: Any = smValue(t)
+    res must beEqualTo(smT: Any)
   }
 
   def checkSMValuePrimitiveList = {
-    val origList = List[Object](1, "a", 2L)
-    val expectedSMValues = List(new SMInt(1L), new SMString("a"), new SMLong(2L))
-    val expectedSMList = new SMList(expectedSMValues)
-    smValue(origList) must beEqualTo(expectedSMList)
+    val origList = List[Object](new Integer(1), "a")
+    val expectedSMValues: List[SMValue[_]] = List(new SMInt(1L), new SMString("a"))
+    val expectedSMList = new SMList(expectedSMValues.asJava)
+    checkSMValue(origList, expectedSMList)
   }
 }
