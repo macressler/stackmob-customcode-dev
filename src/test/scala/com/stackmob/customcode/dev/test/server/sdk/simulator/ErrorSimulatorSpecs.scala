c1775f46392091ebd963ce5d49944acfd06a631e
Aaron Schlesinger <arschles@gmail.com>
2013-05-09 18:38:44 -0700

    add ErrorSimulatorSpecs fixes #32

--- src/test/scala/com/stackmob/customcode/dev/test/server/sdk/simulator/ErrorSimulatorSpecs.scala
+++ src/test/scala/com/stackmob/customcode/dev/test/server/sdk/simulator/ErrorSimulatorSpecs.scala
@@ -6,19 +6,33 @@ package simulator
 
 import org.specs2.{ScalaCheck, Specification}
 import org.specs2.mock.Mockito
+import java.util.concurrent.atomic.AtomicInteger
+import com.stackmob.customcode.dev.server.sdk.simulator.{Frequency, ErrorSimulator, ThrowableFrequency}
+import com.twitter.util.Duration
+import java.util.concurrent.TimeUnit
 
-/**
- * Created by IntelliJ IDEA.
- *
- * com.stackmob.customcode.server.sdk
- *
- * User: aaron
- * Date: 4/17/13
- * Time: 4:02 PM
- */
 class ErrorSimulatorSpecs extends Specification with CustomMatchers with Mockito with ScalaCheck { def is =
   "ErrorSimulatorSpecs".title                                                                                           ^ end ^
+  "ErrorSimulator is responsible for simulating 0 or more errors, represented as ThrowableFrequency"                    ^ end ^
+  "if no simulation, and there are multiple errors, the operation is executed exactly once"                             ! exactlyOnce ^ end ^
   end
-  //TODO: ensure that an operation is only executed once in all cases
+
+  private def simulator(freqs: ThrowableFrequency*): ErrorSimulator = {
+    new ErrorSimulator(freqs.toSeq)
+  }
+
+  private def exactlyOnce = {
+    val atomicInt = new AtomicInteger(0)
+    val sim = simulator(
+      ThrowableFrequency(new Exception("hello world"), Frequency(0, Duration(1, TimeUnit.SECONDS))),
+      ThrowableFrequency(new Exception("hello world"), Frequency(0, Duration(1, TimeUnit.MILLISECONDS)))
+    )
+    val ret = sim.apply {
+      atomicInt.incrementAndGet
+    }
+    val numExec = atomicInt.get() must beEqualTo(1)
+    val res = ret must beEqualTo(1)
+    numExec and res
+  }
 
 }
