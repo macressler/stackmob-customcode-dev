95dcb3e19810041918529e0c05dd63157fe3f244
Aaron Schlesinger <arschles@gmail.com>
2013-05-21 15:45:36 -0700

    adding more functionality to MockJettyRequest

--- src/test/java/com/stackmob/customcode/dev/test/server/MockJettyRequest.java
+++ src/test/java/com/stackmob/customcode/dev/test/server/MockJettyRequest.java
@@ -1,18 +1,24 @@
 package com.stackmob.customcode.dev.test.server;
 
+import org.eclipse.jetty.http.HttpURI;
 import org.eclipse.jetty.server.Request;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MockJettyRequest extends Request {
     private final String methodString;
     private final String uriString;
     private final Map<String, String> headers;
     private final String bodyString;
+
+    private final AtomicBoolean handled = new AtomicBoolean(false);
 
     public MockJettyRequest(String methodString, String uriString, Map<String, String> headers, String bodyString) {
         this.methodString = methodString;
@@ -24,6 +30,20 @@ public class MockJettyRequest extends Request {
     @Override
     public String getMethod() {
         return this.methodString;
+    }
+
+    @Override
+    public HttpURI getUri() {
+        return new HttpURI(uriString);
+    }
+
+    @Override
+    public String getPathInfo() {
+        try {
+            return new URL(uriString).getPath();
+        } catch (MalformedURLException t) {
+            return "/";
+        }
     }
 
     @Override
@@ -44,5 +64,14 @@ public class MockJettyRequest extends Request {
     @Override
     public Enumeration getHeaderNames() {
         return Collections.enumeration(this.headers.keySet());
+    }
+
+    @Override
+    public void setHandled(boolean h) {
+        handled.set(h);
+    }
+
+    public boolean getHandled() {
+        return handled.get();
     }
 }
